879dc9922b2af39d1b2444e8711d4c49dc401916
yuma300 <ywakimoto1s@paperboy.co.jp>
2013-08-15 13:30:45 +0900

    generate した時に引数で与えた名前 + classifier.rb という名前のファイルを app/classifier/ 配下に作成するようにした

--- lib/generators/classifier/classifier_generator.rb
+++ lib/generators/classifier/classifier_generator.rb
@@ -1,3 +1,7 @@
 class ClassifierGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
+
+  def create_classifier_files
+    template 'classifier.rb', File.join('app/classifier', class_path, "#{file_name}_classifier.rb")
+  end
 end
---
+++ lib/generators/classifier/templates/classifier.rb
@@ -0,0 +1,3 @@
+class <%= class_name %>Classifier < ApplicationService
+
+end
